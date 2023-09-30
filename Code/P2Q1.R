30f65b8d21832c08e522a5bd67f7b27bfbc6d6eb
YongfanZhao <111307027+YongfanZhao@users.noreply.github.com>
2023-09-30 16:20:45 -0400

    Update P2Q1.R

--- Code/P2Q1.R
+++ Code/P2Q1.R
@@ -1,8 +1,8 @@
 library(readr)
 
 in_path <- getwd()
-data <- read.table(file.path(in_path, 'Data', 'Raw', 'ck1994_data.dat'))
-colnames <- c('SHEET', 'CHAIN', 'CO_OWNED', 'STATE', 'SOUTHJ', 'CENTRALJ',
+ck1994 <- read.table(file.path(in_path, 'Data', 'Raw', 'ck1994_data.dat'))
+col <- c('SHEET', 'CHAIN', 'CO_OWNED', 'STATE', 'SOUTHJ', 'CENTRALJ',
              'NORTHJ', 'PA1', 'PA2', 'SHORE', 'NCALLS', 'EMPFT', 'EMPPT',
              'NMGRS', 'WAGE_ST', 'INCTIME', 'FIRSTINC', 'BONUS', 'PCTAFF',
              'MEALS', 'OPEN', 'HRSOPEN', 'PSODA', 'PFRY', 'PENTREE', 'NREGS',
@@ -10,4 +10,13 @@ colnames <- c('SHEET', 'CHAIN', 'CO_OWNED', 'STATE', 'SOUTHJ', 'CENTRALJ',
              'EMPPT2', 'NMGRS2', 'WAGE_ST2', 'INCTIME2', 'FIRSTIN2', 'SPECIAL2',
              'MEALS2', 'OPEN2R', 'HRSOPEN2', 'PSODA2', 'PFRY2', 'PENTREE2', 
              'NREGS2','NREGS112')
-             
+            
+colnames(ck1994) <- col
+
+
+# Calculate FTE Employment variable
+ck1994['FTE'] <- ck1994['EMPFT'] + ck1994['NMGRS'] + 0.5*ck1994['EMPPT'] # for wave 1
+ck1994['FTE2'] <- ck1994['EMPFT2'] + ck1994['NMGRS2'] + 0.5*ck1994['EMPPT2'] # for wave 2
+
+
+
